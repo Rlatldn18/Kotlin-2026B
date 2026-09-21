9d9cb3c285841c15eb39d833ae18caa576950b63
Kim Siwoo <k40856874@gmail.com>
2026-09-21 12:30:25 +0900

    0921

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -27,9 +27,62 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        week03Variables()
-        week03Functions()
+//        week03Variables()
+//        week03Functions()
+//        week04Classes()
+        week04Collections()
     }
+}
+
+
+private fun week04Collections() {
+    println("== Kotlin Collections ==")
+
+    val fruits = listOf("apple", "banana", "orange")
+    val mutableFruits = mutableListOf("kiwi", "watermelon")
+
+//    fruits.add("kiwi")  // immutable
+    println("Fruits: $fruits")
+    mutableFruits.add("banana")
+    println("Mutable fruits: $mutableFruits")
+
+    val scores = mapOf("Kim" to 100, "Park" to 97, "Lee" to 99)
+    println("Scores : $scores")
+
+    for (fruit in mutableFruits) {
+        println("I like $fruit")
+    }
+
+    scores.forEach{(x, y) -> println("$x scored $y")}
+    fruits.forEach{fruit -> println("$fruit")}
+}
+
+private fun week04Classes() {
+    println("== Kotlin Classes ==")
+
+    class Student{
+        var name: String = ""
+        var age: Int = 0
+
+        fun introduce(){
+            println("Hi, I'm $name and I'm $age years old")
+        }
+    }
+
+    val student1 = Student()
+    student1.name = "Mirae"
+    student1.age = 21
+    student1.introduce()
+
+    data class Person(val name: String, val age: Int)
+
+    val person1 = Person("Kim", 23)
+    val person2 = Person("Park", 21)
+
+    println("Person1 : $person1")
+    println("Person1 : ${person1.name}")
+    println("Person1 : ${person1.age}")
+    println("Person2 : $person2")
 }
 
 private fun week03Variables() {
@@ -43,9 +96,9 @@ private fun week03Variables() {
     println("Course : $courseName")
     println("Week : $week")
 
-    println("============== kotlin Variables ==============")
+    println("========= Kotlin Variables =========")
 
-    // val (immutable) vs var (mutable)
+    // val(immutable) vs var(mutable)
     val name = "Android"
     var version = 8
 
@@ -55,17 +108,16 @@ private fun week03Variables() {
     val height: Double = 177.7
     val isStudent: Boolean = false
 
-    println("Age : $age, Height : $height, Student : $isStudent")
+    println("Age: $age, Height: $height, Student: $isStudent")
 
 //    var nickname:String = null
     var nickname:String? = null
-    nickname = "mirea"
-    println("NickName : $nickname ${nickname?.length}")
-
+    nickname = "mirae"
+    println("Nickname: $nickname ${nickname?.length}")
 }
 
 private fun week03Functions(){
-//    println("Week 02: Functions")
+//    println("Week 03: Functions")
 //
 //    fun greet(name: String) = "Hello, $name!"
 //
@@ -73,20 +125,19 @@ private fun week03Functions(){
 
     println("== Kotlin Functions ==")
 
-    fun greet(name: String): String {
-        return "Hello, $name!"
+    fun printAll(vip: Boolean, name: String){
+        println("$vip, $name")
     }
 
-    fun add(a: Int, b: Int) = a + b
-
-    fun introduce(name: String, age: Int = 19){
-        println("My name is $name and I'm $age years old")
+    fun printMany(vararg msg: String){  // variable arguments
+        for(m in msg) println(m)
     }
 
-    println(greet("Kotlin"))
-    println("Sum: ${add(5, -71)}")
-    introduce("Kim", 7)
-    introduce("Park")
+//    printAll("dy", true)
+    printAll(true, "dy")
+    printAll(name = "mirae", vip = true)  // named arguments
+
+    printMany("A", "B", "C", "D")
 }
 
 @Composable
